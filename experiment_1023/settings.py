965da66a63ef2adbf8f9743c1c35b612b0d9df3a
360er0 <lomero@tlen.pl>
2017-12-17 22:05:16 +0100

    Update settings.py

--- experiment_1023/settings.py
+++ experiment_1023/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'tw_au)npp9ljb&k9y^1zpd*rzb#elc-!tmdyc)w2j&1*oq^s9x'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://byrolew.pythonanywhere.com']
 
 
 # Application definition
